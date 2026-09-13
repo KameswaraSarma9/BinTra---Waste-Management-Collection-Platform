950b8d4d643242bfbedeb0fa9db97e44d9077e50
Renduchintala Venkata  Kameswara Sarma <161705989+KameswaraSarma9@users.noreply.github.com>
2026-09-14 00:23:32 +0530

    Delete server directory

--- server/routes/rewardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getMyRewards, getMyTransactions } = require("../controllers/rewardController");
-const { protect } = require("../middleware/auth");
-
-router.get("/", protect, getMyRewards);
-router.get("/transactions", protect, getMyTransactions);
-
-module.exports = router;
